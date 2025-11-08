560a912563747d55f3a8450876902d56f5854bd8
Flyan <guilhermeflyan.22@gmail.com>
2025-11-08 13:43:30 -0300

    Correction of Hydration Error

--- src/app/components/Details.tsx
+++ src/app/components/Details.tsx
@@ -5,7 +5,7 @@ type DetailsProps = {
 export default function Details({comandos}: DetailsProps) {
     return(
         <div className="mt-2.5 bg-[#434343] rounded-xl h-[195px] w-[280px] lg:h-[480px] lg:w-[350px] xl:w-[610px]  2xl:h-[570px] 2xl:w-[880px] overflow-y-auto">
-            <p className="text-white p-5 text-justify whitespace-pre-wrap">{parseComandos(comandos)}</p>
+            <div className="text-white p-5 text-justify whitespace-pre-wrap">{parseComandos(comandos)}</div>
         </div>
     );
 }
