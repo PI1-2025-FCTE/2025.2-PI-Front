eaf6bb48439206ba15725a4107789dd29016bb90
YasminDayrell <yasminstudy2@gmail.com>
2025-11-22 13:42:39 -0300

    adding a new test

--- src/cypress/e2e/US3.cy.ts
+++ src/cypress/e2e/US3.cy.ts
@@ -7,6 +7,16 @@ describe("US3 - Conexão com dispositivos", () => {
     cy.contains("Nenhum dispositivo conectado").should("be.visible");
   });
 
+  it("Deve impedir o envio de rota caso não haja ESP'", () => {
+    cy.contains("Avançar").click();
+    cy.get("input[type=number]").clear().type("1250");
+    cy.contains("Virar à direita").click();
+
+    cy.contains("Enviar").should("be.disabled")
+
+  });
+
+
   it("Deve conectar se ouver ESP disponível", () => {
     cy.contains("Selecionar").should("be.visible").click();
 
