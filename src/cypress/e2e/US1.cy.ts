ff387241cd7350d1695a8c477385ae4123ba9f77
YasminDayrell <yasminstudy2@gmail.com>
2025-11-24 17:09:34 -0300

    fixing test bugs

--- src/cypress/e2e/US1.cy.ts
+++ src/cypress/e2e/US1.cy.ts
@@ -1,17 +1,11 @@
 describe("Fluxo completo de visualização de trajetos", () => {
 
-    //aguardando implementação do gráfico
   it("Deve abrir a sidebar, selecionar um percurso e exibir os detalhes", () => {
     cy.visit("/");
 
     // Abre a sidebar 
     cy.get("header button").first().click();
 
-    // Espera o carregamento 
-    cy.wait("@getTrajetos");
-
-    //  Verifica se a sidebar ficou visível e contém os botões de percurso
-    cy.contains("PERCURSO 1").should("be.visible");
-    cy.contains("PERCURSO 2").should("be.visible");
+    cy.contains(/^PERCURSO \d+$/).should("be.visible");
   });
 });
