3179619568523f0fd3c1ea9027aa2d657a6f7bac
YasminDayrell <yasminstudy2@gmail.com>
2025-11-24 17:10:07 -0300

    fixing test bugs

--- src/cypress/e2e/US2.cy.ts
+++ src/cypress/e2e/US2.cy.ts
@@ -1,22 +1,18 @@
 describe("Fluxo completo de visualização de trajetos", () => {
 
     //aguardando implementação do gráfico
-  it("Deve abrir a sidebar, selecionar um percurso e exibir os detalhes", () => {
-    cy.visit("/");
+    it("Deve abrir a sidebar, selecionar um percurso e exibir os detalhes", () => {
+      cy.visit("/");
 
-    // Abre a sidebar 
-    cy.get("header button").first().click();
+      cy.get("header button").first().click();
 
-    //  Verifica se a sidebar ficou visível e contém os botões de percurso
-    cy.contains("PERCURSO 1").should("be.visible");
-    cy.contains("PERCURSO 2").should("be.visible");
+      cy.contains(/^PERCURSO \d+$/).should("be.visible");
 
-    // Clica no primeiro percurso
-    cy.contains("PERCURSO 1").click();
+      // clica no primeiro percurso encontrado
+      cy.contains(/^PERCURSO \d+$/).last().click();
 
+      cy.contains("DETALHES DA TRAJETÓRIA").should("be.visible");
+      cy.contains("MAPA DO TRAJETO").should("be.visible");
+    });
 
-    cy.contains("DETALHES DA TRAJETÓRIA").should("be.visible");
-    cy.contains("MAPA DO TRAJETO").should("be.visible");
-
-  });
 });
